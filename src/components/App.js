c42670d1b328f6c924431a04691d2e07e0a15390
Jai Pratap Singh <43641473+jai13598@users.noreply.github.com>
2021-08-18 18:18:26 +0530

    solve the question

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,25 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
 class App extends Component {
     constructor(props) {
-		super(props);
-	};
+        super(props);
+
+        this.state = {
+            pHidden: false,
+        }
+    }
 
     render() {
-    	return(
-    		<div id="main">
-				{ /* Do not remove this main div!! */ }
-    		</div>
-    	);
+        return ( 
+            <div id = "main" >
+             <button id="click" onClick={()=> {this.setState({pHidden:true});}}>show</button>
+             {this.state.pHidden? (<p id="para">
+             Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy
+             </p>):null}   
+            </div >
+        );
+        }
     }
-}
 
-
-export default App;
-
+    export default App;
